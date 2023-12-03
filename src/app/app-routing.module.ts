4f2e0f45c646c4caefa3b88adbe3f2c802911e56
Pablo Jimenez <Pablo.jimenez2@live.u-tad.com>
2023-12-03 20:44:03 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModeloComponent } from './components/modelo/modelo.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'modelo', component:ModeloComponent},
+  {path:'home', component:LandingPageComponent},
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'**', component:ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ModeloComponent } from './components/modelo/modelo.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingPageComponent,
+    ModeloComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
